104f9a2234ecfac74f9cd2195f28eacc13291983
Matt Hughes <matt.c.hughes@gmail.com>
2004-03-07 05:46:47 +0000

    Constants

---
+++ src/Constants.hh
@@ -0,0 +1,15 @@
+// Defines a bunch of useful constants that will be handy
+
+// Prefer static const over #define. 
+
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include "Types.hh"
+
+static const field_t PI = 3.14159;
+static const field_t EPS_0 = 8.854187817e-12;
+static const field_t MU_0 = 1.2566e-6;
+
+
+#endif // CONSTANTS_H
