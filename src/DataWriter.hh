aac2008d3c7fdb87beb4e5bb684a8e0186d7a17a
Matt Hughes <matt.c.hughes@gmail.com>
2004-03-12 22:12:59 +0000

    Result overhaul

--- src/DataWriter.hh
+++ src/DataWriter.hh
@@ -8,46 +8,6 @@
 
 using namespace std;
 
-class DataWriter;
-
-/**
- * A variable which defines the name, dimensionality, and size of
- * the data to be output. 
- */
-class Variable
-{
-private:
-  Variable();
-
-protected:
-  string name_;
-
-  DataWriter dw_; /**< Output goes here */
-
-public:
-  Variable(DataWriter &dw, string name)
-    : name_(name), dw_(dw)
-  {}
-
-  virtual Variable() = 0;
-};
-
-/**
- * A variable for 4d (x,y,z,t) data
- */
-class Variable_4d : public Variable
-{
-private:
-  Variable_4d();
-protected:
-  region r_;
-
-public:
-  Variable_4d(DataWriter &dw, string name, region r)
-    : Variable(dw, name), r_(r)
-  {}
-};
-
 /**
  * An abstract base class that can be subclassed to create objects
  * which save data to disk, or perform other output
@@ -55,27 +15,120 @@ public:
  * writers may only be able to support one. Variables have dimensions,
  * units, and names. DataWriter's must support at least one
  * dimensional data.
+ *
+ * \bug add_variable doesn't check if a varable by that name already exists!
  */
-template <class T>
 class DataWriter 
 {
 private:
-protected:
-    
-  map<string, *Variable> variables_;
-  
 public:
 
-  DataWriter() {}
+  /**
+   * A variable which defines the name, dimensionality, and size of
+   * the data to be output. 
+   */
+  class Variable
+  {
+  private:
+    Variable();
+
+  protected:
+    string name_;
+
+    DataWriter dw_; /**< Output goes here */
+
+  public:
+    Variable(DataWriter &dw, string name)
+      : name_(name), dw_(dw)
+    {}
+
+    virtual ~Variable() = 0;
+  };
+
+  /**
+   * A variable for 4d (x,y,z,t) data
+   */
+  class Variable_4d : public Variable
+  {
+  private:
+    Variable_4d();
+  protected:
+    region r_;
+
+    /** 
+     * Implementing classes must implment this to do the actual saving. 
+     */
+    virtual void write_point(unsigned int x, unsigned int y,
+                             unsigned int z, field_t val) = 0;
+
+  public:
+    Variable_4d(DataWriter &dw, string name, region r)
+      : Variable(dw, name), r_(r)
+    {}
+
+    virtual ~Variable_4d() = 0;
+
+    /**
+     * Save a point of data. Simple. Slow. Sad. If this DataWriter is
+     * not on rank 0, the data is transmitted to rank 0. And then the
+     * write_point function is called by rank 0. 
+     *
+     * @param x x coord
+     * @param y y coord
+     * @param z z coord
+     * @param val value
+     */ 
+    void save_point(unsigned int x, unsigned int y,
+                    unsigned int z, field_t val);
+  };
+
+protected:
+  unsigned int max_vars_; /**< Maximum number of variable for this
+                             file */
+
+  map<string, *Variable> variables_;
+  
+  int rank_;  /**< MPI Rank of this process */
+  int size_; /**< Number of processes in MPI comm. */
+
+private:
+  DataWriter()
+  {}
+
+public:
+  DataWriter(int rank, int size) 
+    : max_vars_(0), rank_(rank), size_(size) 
+  {}
+
   virtual ~DataWriter() = 0;
 
   /**
-   * Do something with a chunk of data.
+   * Initialize this object
+   */
+  virtual void init() = 0;
+
+  /**
+   * Deinit
+   */
+  virtual void deinit() = 0;
+
+  /**
+   * Returns the rank
+   */
+  inline int get_rank()
+  {
+    return rank_;
+  }
+
+  /**
+   * Returns the size
    *
-   * @param len number of items
-   * @param data data to save
-   */ 
-  virtual void save_data(unsigned int len, T data) = 0;
+   * @return an int; number of MPI processes
+   */
+  inline int get_size()
+  {
+    return size_;
+  }
 
   /**
    * Add a 4d variable; x, y, z, and time! The x, y, and z axis are
@@ -85,7 +138,8 @@ public:
    * @param name a variable name
    * @param r describes the region to output
    */
-  Variable_4d* add_4d_variable(string name, region r);
+  Variable_4d& add_4d_variable(string name, region r);
+
 };
 
 #endif // DATA_WRITER_H
--- src/Result.hh
+++ src/Result.hh
@@ -3,16 +3,87 @@
 
 #include <string>
 #include "Types.hh"
-#include "DataWriter.hh"
 #include "Grid.hh"
 
 using namespace std;
+
+/**
+ * A wrapper for a MPI derived data type, a pointer, and an int
+ * indicating the number of items. 
+ */
+class Data {
+private:
+protected:
+  MPI_Datatype type_;
+  void *ptr_;
+  unsigned int num_;
+
+public:
+  Data();
+  ~Data();
+
+  /**
+   * Used to set the data type.
+   */
+  inline void set_datatype(MPI_Datatype &type)
+  {
+    type_ = type;
+  }
+
+  /**
+   * Get the data type
+   */
+  inline MPI_Datatype get_datatype()
+  {
+    return type_;
+  }
+
+  /** 
+   * Get the pointer
+   */
+  inline void *get_ptr()
+  {
+    return ptr_;
+  }
+
+  /**
+   * get the number of items
+   */
+  inline unsigned int get_num()
+  {
+    return num_;
+  }
+
+  /**
+   * Set the pointer.
+   */
+  inline void set_ptr(void *ptr)
+  {
+    ptr_ = ptr;
+  }
+
+  /**
+   * Set the number of items
+   */
+  inline void set_num(unsigned int num)
+  {
+    num_ = num;
+  }
+};
 
 /**
  * An abstract base class used to implement results. This class
  * recieves a reference to a grid and a reference to a DataWriter. It
  * takes data from the grid, performs calculations, and then calls
  * functions in the DataWriter to save the data. 
+ *
+ * The method get_result() MUST be implemented. Is intended to be called
+ * after every the updates for each time step have been computed. 
+ *
+ * get_result(), returns a structure containing an MPI derived
+ * datatype describing the data, a pointer to the data, and the number
+ * of elements in the result. If there is no data to be written, then
+ * the number of elements is zero. 
  */
 class Result
 {
@@ -28,9 +99,10 @@ public:
    * Looks at the grid and produces output
    *
    * @param grid a reference to a Grid object
-   * @param writer a reference to a DataWriter object
+   * @return a data object, which contains an MPI derived data type,
+   * a pointer, and the number of items in the result. 
    */
-  virtual void produce_output(Grid &grid, DataWriter &dw) = 0;
+  virtual Data get_result(Grid &grid) = 0;
 
 };
 
