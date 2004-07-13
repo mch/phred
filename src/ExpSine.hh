512ced946fcdbec1785b460ed566de005b2d3404
Matt Hughes <matt.c.hughes@gmail.com>
2004-07-13 00:28:08 +0000

    yeehaw

---
+++ src/ExpSine.hh
@@ -0,0 +1,90 @@
+/* 
+   phred - Phred is a parallel finite difference time domain
+   electromagnetics simulator.
+
+   Copyright (C) 2004 Matt Hughes <mhughe@uvic.ca>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
+*/
+
+#ifndef EXCITE_EXPSINE_H
+#define EXCITE_EXPSINE_H
+
+#include "SourceFunction.hh"
+#include "Constants.hh"
+
+#include <math.h>
+
+class ExpSine : public SourceFunction
+{
+public:
+  /**
+   * Produces a sine function that is windows by an exponential
+   * function so that the initial discontinuity is not so bad. 
+   *
+   * @param grid the grid to which the function is being applied. Only for
+   * things such as delta_t, the result of this function is applied in
+   * the excite() function (in order to factor out the loop). 
+   *
+   * @param time_step the time at which to apply the excitation
+   *
+   * @return the value of the excitation
+   */
+  field_t source_function(const Grid &grid, unsigned int time_step);
+
+  ExpSine();
+  ExpSine(float frequency);
+  ~ExpSine();
+
+  /**
+   * Set the frequency of the excitation in Hz. 
+   */
+  inline void set_frequency(float frequency)
+  {
+    period_ = 1 / frequency;
+    omega_ = 2 * PI * frequency;
+  }
+  
+  /**
+   * Set the amplitude of the excitation.
+   */ 
+  inline void set_amplitude(float ampl)
+  {
+    ampl_ = ampl;
+  }
+
+  /**
+   * Returns the frequency of the excitation in Hz. 
+   */
+  inline float get_frequency()
+  {
+    return 1/period_;
+  }
+
+  /**
+   * Returns the amplitude of the signal
+   */
+  inline float get_amplitude()
+  {
+    return ampl_;
+  }
+
+protected:
+  float period_;
+  float omega_;
+  float ampl_;
+};
+
+#endif // EXCITE_EXPSINE_H
---
+++ src/WaveguideExcitation.hh
@@ -0,0 +1,89 @@
+/* 
+   phred - Phred is a parallel finite difference time domain
+   electromagnetics simulator.
+
+   Copyright (C) 2004 Matt Hughes <mhughe@uvic.ca>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
+*/
+
+#ifndef WAVEGUIDE_EXCITATION_H
+#define WAVEGUIDE_EXCITATION_H
+
+#include "WindowedExcitation.hh"
+
+/**
+ * A wave guide excitation. Exciates TE_{nm} modes across a retangular
+ * window.
+ */ 
+class WaveGuideExcitation : public WindowedExcitation
+{
+public:
+  WaveGuideExcitation(SouceFunction *sf)
+    : WindowedExcitation(sf)
+  {}
+
+  ~WaveGuideExcitation()
+  {}
+
+  virtual field_t window(region_t r, unsigned int x, unsigned int y, 
+                         unsigned int z);
+
+  /**
+   * Set the mode to excite. 
+   *
+   * @param n the number of half wavelengths in the long dimension
+   * @param m the number of half wavelengths in the short dimension
+   */
+  void set_mode(unsigned int n, unsigned int m)
+  {
+    mode_n_ = n;
+    mode_m_ = m;
+  }
+
+  /** 
+   * Set the excitation size, location, and orientation.
+   */
+  void set_wg_excitation(unsigned int x, unsigned int y, unsigned int z,
+                         unsigned int width, unsigned int height, 
+                         Axis a)
+  {
+    x_ = x;
+    y_ = y;
+    z_ = z;
+    width_ = width;
+    height_ = height;
+    axis_ = a;
+  }
+
+  /**
+   * From the window size, placement, and orientation, construct a
+   * suitable region and set the mode numbers for each axis. Make
+   * sure everything is inside the local region, etc.
+   */ 
+  virtual void init(const Grid &grid);
+
+protected:
+  unsigned int mode_n_;
+  unsigned int mode_m_;
+
+  unsigned int x_, y_, z_;
+  unsigned int width_, height_;
+  Axis axis_;
+
+private:
+};
+
+#endif WAVEGUIDE_EXCITATION_H
