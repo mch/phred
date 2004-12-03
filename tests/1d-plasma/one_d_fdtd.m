c270e78e4824993cb21dc1ee10728983183b8567
Matt Hughes <matt.c.hughes@gmail.com>
2004-12-03 20:07:44 +0000

    ugg

--- tests/1d-plasma/one_d_fdtd.m
+++ tests/1d-plasma/one_d_fdtd.m
@@ -16,14 +16,16 @@ sxm1 = ex;
 sxm2 = ex;
 
 material = ones(len, 1);
-material(25:135) = 2;
+material(80:135) = 2;
 
 eps = [1 1] * 8.854185336732027e-12;
 mu = [1 1] * 1.256637061435917e-06;
 sigma = [0 0];
 sigmastar = [0 0];
-plasma_f = [0 1.85e+15];
-collision_f = [0 1.4e+14];
+%plasma_f = [0 1.85e+15];
+%collision_f = [0 1.4e+14];
+plasma_f = [0 5.8e15];
+collision_f = [0 107.5e12];
 
 Ca = (1 - (sigma * dt * 0.5) ./ eps) ./ ...
      (1 + (sigma * dt * 0.5) ./ eps);
@@ -40,7 +42,7 @@ B = (plasma_f .* 2 .* pi) .^ 2 .* (dt ./ collision_f);
 ex40 = [];
 ex60 = [];
 
-DFTfreqs = [5e12:5e12:700e12];
+DFTfreqs = [300e12:10e12:800e12];
 ex40DFT_r = zeros(length(DFTfreqs), 1);
 ex40DFT_i = zeros(length(DFTfreqs), 1);
 ex60DFT_r = zeros(length(DFTfreqs), 1);
@@ -56,7 +58,8 @@ for idx = [1:1000]
                                        hy, A, B, dx, sx, sxm1, sxm2);
   
   %temp = gaussm(idx, 500e12, 4000e12, dt);
-  temp = gaussm(idx, 200e12, 100e12, dt);
+  %temp = gaussm(idx, 200e12, 100e12, dt);
+  temp = gaussm(idx, 200e12, 500e12, dt);
   %temp = 100 * gauss(idx, 200e12, dt);
   ex(20) = ex(20) + temp;
 
@@ -74,7 +77,7 @@ for idx = [1:1000]
   ex(1) = ex2(3);
   ex(149) = ex98(3);
   
-  plot([1:len], ex); %, [1:len], hy);
+  plot(ex(75:len)); %, [1:len], hy);
   %pause(0.1);
   %legend('Ex', 'Hy');
   %pause(0.25);
