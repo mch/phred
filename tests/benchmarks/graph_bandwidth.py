c521eeb6b1cbe5e9b93b740a4db343968314d55e
Matt Hughes <matt.c.hughes@gmail.com>
2005-06-21 17:29:41 +0000

    Minor notes, put multiple bandwidth graphs on one...

--- tests/benchmarks/graph_bandwidth.py
+++ tests/benchmarks/graph_bandwidth.py
@@ -1,7 +1,7 @@
 from pyx import *
 import sys
 
-g = graph.graphxy(width=10, key=graph.key.key(pos="tl"),
+g = graph.graphxy(width=12, height=9, key=graph.key.key(pos="tl"),
                   x=graph.axis.log(title="Message Size (bytes)"),
                   y=graph.axis.linear(title="Bandwidth (MB/s)"))
 
@@ -9,11 +9,29 @@ colours = [color.rgb.red, color.rgb.green, color.rgb.blue]
 titles = ["xy plane", "yz plane", "zx plane"]
 i=0
 
-for f in sys.argv[1:]:
-    print "graphing %s..." % f
-    data = graph.data.file(f, title=titles[i],
-                           x="$1**2", y="$3/1e6")
-    g.plot(data, [graph.style.line([ colours[i] ])])
-    i = i + 1
+if (len(sys.argv) == 4):
+    for f in sys.argv[1:]:
+        #print "graphing %s..." % f
+        data = graph.data.file(f, title=titles[i],
+                               x="$1**2", y="$3/1e6")
+        g.plot(data, [graph.style.line([ colours[i] ])])
+        i = i + 1
+
+if (len(sys.argv) == 7):
+    for f in sys.argv[1:4]:
+        #print "graphing %s..." % f
+        data = graph.data.file(f, title=titles[i],
+                               x="$1**2", y="$3/1e6")
+        g.plot(data, [graph.style.line([ colours[i] ])])
+        i = i + 1
+
+    i = 0
+    for f in sys.argv[4:7]:
+        #print "graphing %s..." % f
+        data = graph.data.file(f, title=titles[i],
+                               x="$1**2", y="$3/1e6")
+        g.plot(data, [graph.style.line([ colours[i],
+                                         style.linestyle.dashed ])])
+        i = i + 1
 
 g.writeEPSfile("graph.eps")
